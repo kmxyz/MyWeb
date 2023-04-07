8efbc070d276f97d225390a78d1b31b0fdd3bf41
Mark <markxiao66@gmail.com>
2023-04-07 11:15:23 -0300

    add main page

---
+++ src/pages/main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="bg-secondary-subtle">
+                
+            </div>
+        );
+    }
+}
+ 
+export default MainPage;
