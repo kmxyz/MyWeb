0b21bc7ad8db69f90cbbb5f9f3ec904950632e86
Mark <markxiao66@gmail.com>
2023-04-12 13:36:00 -0300

    fixing bugs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,28 +4,31 @@ import logo from '../pic/logo.JPG';
 
 class NavBar extends Component {
     state = {  
-      navBarItem : ["Home","About Me","Skill","Experience","Contact Me"]
+      navBarItem : ["Home","About Me","Skill","Experience","Contact Me"],
+      sectionPage : ['/section1','/section2','/section3',"/section4","/section5"]
     }
     render() { 
-        return (
-          <div className='bg-secondary-subtle ' style={{ position: "sticky", top: 0 }}>
-            <nav className="navbar" >
-              <div className="container-fluid">               
-                <a className="navbar-brand fs-2" href="#">
-                  <img src={logo} style={{marginLeft : "20px"}} alt="Logo" width="40" height="40" className="d-inline-block align-text-top rounded-3"/>
-                  <span className='border-start border-5'>Devloper world</span>
-                </a>
-                <ul className="nav">
-                  {this.state.navBarItem.map((item) =>
-                  <li key={item} style={{ marginLeft: "30px"}}className="nav-item" >
-                    <a className="nav-link link-dark fs-5"  href="#">
-                      {item}
-                    </a>
-                  </li>)}
-                </ul>  
-              </div>
-            </nav>
-          </div>
+        return (        
+           
+          <nav className="navbar navbar-expand-lg fixed-top shadow p-3 mb-5 bg-body-tertiary rounded" >
+            <div className="container-fluid">               
+              <a className="navbar-brand fs-2" href="#">
+                <img src={logo} style={{marginLeft : "20px"}} alt="Logo" width="40" height="40" className="d-inline-block align-text-top rounded-3"/>
+                <span className='border-start border-5'>Devloper world</span>
+              </a>
+              <ul className="nav">
+                {this.state.navBarItem.map((item) =>
+                <li key={item} style={{ marginLeft: "30px"}}className="nav-item" >
+                  <a 
+                    className="nav-link link-dark fs-5"  
+                    href = {this.state.sectionPage[this.state.navBarItem.indexOf(item)]}
+                  >
+                    {item}
+                  </a>
+                </li>)}
+              </ul>  
+            </div>
+          </nav>
         );
 
     }
