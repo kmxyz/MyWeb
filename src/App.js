b2aa0e0eb3d4860925d13ab545192086922ce367
Mark <markxiao66@gmail.com>
2023-04-18 16:59:36 -0300

    add more information for about me

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/navbar';
-import MainPage from './pages/main';
-import AboutMe from './pages/aboutme';
-import { Route, Router, Routes} from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import MainPage from "./pages/main";
+import AboutMe from "./pages/aboutme";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path = "/" Component ={MainPage}/>
-        <Route path = "/about" Component ={AboutMe}/>
-      </Routes>
+      <MainPage />
+      <AboutMe />
     </div>
   );
 }
