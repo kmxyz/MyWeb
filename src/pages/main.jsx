656723ce29b6d650af9702148a1aef9741a602a1
Mark <markxiao66@gmail.com>
2023-04-24 19:38:22 -0300

    small margin change in main page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -9,7 +9,7 @@ class MainPage extends Component {
           <div className="text-sm-start text-center my-5 p-sm-5 p-4">
             <h5>Web Devloper</h5>
             <h1>Yunzhong(Mark) Xiao</h1>
-            <p className="p-sm-2 p-md-0" style={{ width: "26em" }}>
+            <p className="p-sm-1 p-md-0" style={{ width: "26em" }}>
               Enjoy solving complex problems both independently and
               collaboratively. Enable to write high-quality and clean code.
               Motivated to learn new web design and machine learning technology.
