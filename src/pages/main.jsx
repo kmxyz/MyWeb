bf4e723e05fd3177921528e06f160b12efb8a81c
Mark <markxiao66@gmail.com>
2023-04-24 19:35:32 -0300

    changed margin in main page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -5,18 +5,16 @@ class MainPage extends Component {
   render() {
     return (
       <main id="/" className="">
-        <div className="position-absolute my-5 p-md-5">
-          <div className="text-md-start text-sm-center my-5 p-md-5">
+        <div className="position-absolute my-5 p-sm-5">
+          <div className="text-sm-start text-center my-5 p-sm-5 p-4">
             <h5>Web Devloper</h5>
             <h1>Yunzhong(Mark) Xiao</h1>
-            <p className="p-2" style={{ width: "26em" }}>
+            <p className="p-sm-2 p-md-0" style={{ width: "26em" }}>
               Enjoy solving complex problems both independently and
               collaboratively. Enable to write high-quality and clean code.
               Motivated to learn new web design and machine learning technology.
             </p>
-            <button style={{ marginTop: "20px" }} className="btn btn-dark">
-              resume
-            </button>
+            <button className="btn btn-dark">resume</button>
           </div>
         </div>
         <span>
