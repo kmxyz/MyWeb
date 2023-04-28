359c948894298b76e3e2d18b9080a91dbe69e11d
Mark <markxiao66@gmail.com>
2023-04-27 22:06:39 -0300

    add all pic for skill page

--- src/pages/skill.jsx
+++ src/pages/skill.jsx
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
-import Test from "../pic/lang/test.JPG";
+import Html from "../pic/lang/html.png";
+import JavaScript from "../pic/lang/JavaScript.png";
+import Css from "../pic/lang/css.png";
+import Git from "../pic/lang/git.png";
+import Python from "../pic/lang/python.png";
+import Java from "../pic/lang/java.png";
+
+import MySql from "../pic/software/mysql.png";
+import Github from "../pic/software/github.png";
+import VisualStudio from "../pic/software/visualstudio.svg";
+import AzureDevOps from "../pic/software/AzureDevOps.png";
+import Firebase from "../pic/software/Firebase.png";
+import Docker from "../pic/software/Docker.jpg";
 
 class Skill extends Component {
   state = {
     imgLanguageInfo: [
-      { name: "html", url: Test },
-      { name: "css", url: Test },
-      { name: "javascript", url: Test },
-      { name: "java", url: Test },
-      { name: "python", url: Test },
-      { name: "git", url: Test },
+      { name: "html", url: Html },
+      { name: "css", url: Css },
+      { name: "javascript", url: JavaScript },
+      { name: "java", url: Java },
+      { name: "python", url: Python },
+      { name: "git", url: Git },
     ],
     imgSoftwareInfo: [
-      { name: "MySQL Workbench", url: Test },
-      { name: "GitHub", url: Test },
-      { name: "Visual Studio Code", url: Test },
-      { name: "Azure DevOps", url: Test },
-      { name: "Firebase", url: Test },
-      { name: "Docker", url: Test },
+      { name: "MySQL Workbench", url: MySql },
+      { name: "GitHub", url: Github },
+      { name: "Visual Studio Code", url: VisualStudio },
+      { name: "Azure DevOps", url: AzureDevOps },
+      { name: "Firebase", url: Firebase },
+      { name: "Docker", url: Docker },
     ],
   };
 
@@ -36,14 +48,14 @@ class Skill extends Component {
               height: "300px",
             }}
           >
-            <ul className="d-flex justify-content-center py-sm-5 h-100 align-items-center flex-wrap pe-4">
-              {this.state.imgLanguageInfo.map((item) => (
+            <ul className="d-flex justify-content-center py-5 h-100 align-items-center flex-wrap pe-4">
+              {this.state.imgSoftwareInfo.map((item) => (
                 <li key={item.name}>
                   <img
                     src={item.url}
                     alt={item.name}
-                    className="img-fluid rounded-circle m-sm-2 m-lg-3 m-1"
-                    style={{ width: "100px" }}
+                    className="img-fluid rounded-2 m-sm-3 m-lg-4 m-2"
+                    style={{ width: "80px" }}
                   />
                 </li>
               ))}
@@ -62,14 +74,14 @@ class Skill extends Component {
               height: "300px",
             }}
           >
-            <ul className="d-flex justify-content-center py-sm-5 h-100 align-items-center flex-wrap pe-4">
+            <ul className="d-flex justify-content-center py-5 h-100 align-items-center flex-wrap pe-4">
               {this.state.imgLanguageInfo.map((item) => (
                 <li key={item.name}>
                   <img
                     src={item.url}
                     alt={item.name}
-                    className="img-fluid rounded-circle m-sm-2 m-lg-3 m-1"
-                    style={{ width: "100px" }}
+                    className="img-fluid rounded-2 m-sm-3 m-lg-4 m-2"
+                    style={{ width: "80px" }}
                   />
                 </li>
               ))}
