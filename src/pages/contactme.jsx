fa3960fb4624cf840d6f339da65d61043fa95422
Mark <markxiao66@gmail.com>
2023-05-23 10:57:54 -0300

    delete the phone number in contactme page

--- src/pages/contactme.jsx
+++ src/pages/contactme.jsx
@@ -24,31 +24,20 @@ class ContactMe extends Component {
           <p className="text-secondary ">Let me be part of your team</p>
         </div>
         <div className="col mx-sm-5">
-          <div className="row ">
-            <div className="col col-xs-2 ps-5 ms-4 ps-sm-0 ms-sm-0 d-flex align-items-center ">
-              <span className=" shadow rounded-circle p-3 m-3">
-                <i className="fa fa-phone fa-lg text-primary "></i>
-              </span>
-              <div className="text-start pt-2">
-                <h4>Phone</h4>
-                <p className="text-secondary">902-579-9586</p>
-              </div>
-            </div>
-            <div className="col ps-5 ms-4 ps-sm-0 ms-sm-0 d-flex align-items-center">
-              <span className=" shadow rounded-circle p-3 m-3">
-                <i className="fa fa-phone fa-lg text-primary"></i>
-              </span>
-              <div className="text-start">
-                <h4>E-mail</h4>
-                <a
-                  href="mailto: markxiao66@gmail.com"
-                  className={" text-decoration-none " + this.state.colors}
-                  onMouseEnter={this.headleOn}
-                  onMouseLeave={this.headleLeave}
-                >
-                  markxiao66@gmail.com
-                </a>
-              </div>
+          <div className="d-flex align-items-center justify-content-md-end ps-5 ms-2 ps-sm-0">
+            <span className=" shadow rounded-circle p-3 m-3">
+              <i className="fa fa-envelope fa-lg text-primary"></i>
+            </span>
+            <div className="text-start">
+              <h4>E-mail</h4>
+              <a
+                href="mailto: markxiao66@gmail.com"
+                className={" text-decoration-none " + this.state.colors}
+                onMouseEnter={this.headleOn}
+                onMouseLeave={this.headleLeave}
+              >
+                markxiao66@gmail.com
+              </a>
             </div>
           </div>
         </div>
